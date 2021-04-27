c6711186c8bc369925f51b8a8aad9e3df452eeae
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-27 23:59:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(x => x * -1);
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(x => x);
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(x => x*2);
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(x => x*x);
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue){ return accumulator + currentValue }
+
+    return sourceArray.reduce(reducer, startingPoint)
+}
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){
+        if(Boolean(accumulator)===true && Boolean(currentValue)===true)
+          return true;
+        else {
+          return false;
+        }
+    }
+    return sourceArray.reduce(reducer);
+}
+function reduceToAnyTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){
+        if(Boolean(accumulator)===true || Boolean(currentValue)===true)
+          return true;
+        else {
+          return false;
+        }
+    }
+    return sourceArray.reduce(reducer);
+}
