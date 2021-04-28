14b91b2e6ed9eeb14a1240210c1da10793d0b573
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-28 00:51:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code here
+<<<<<<< HEAD
 function mapToNegativize(sourceArray){
     return sourceArray.map(x => x * -1);
 }
@@ -42,3 +43,15 @@ function reduceToAnyTrue(sourceArray){
     }
     return sourceArray.reduce(reducer);
 }
+=======
+let sourceArray=[2,4,6];
+const mapToNegativize = sourceArray.map(x => x * -1);
+const mapToNoChange = sourceArray.map(x => x );
+const mapToDouble = sourceArray.map(x => x * 2);
+const mapToSquare = sourceArray.map(x => x * x);
+console.log(mapToSquare);
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const reduceToTotal=sourceArray.reduce(reducer, 0);
+>>>>>>> d846831ac8711f964bf8e38a8322c89a4ec5d5c1
